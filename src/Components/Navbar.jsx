0e6accf8fdc323672e844993f5e64571f5896447
poluslg <prahladbiswas08@gmail.com>
2023-04-17 12:54:06 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import "./css/Navbar.css"
 
+
+
+
 export default function Navbar() {
-  return (
-    <>
-    <div className="navwraper">
-        <div className="n-left">
-            <div className="n-name">
-                Polu
+    return (
+        <>
+            <div className="navwraper">
+                <div className="n-left">
+                    <div className="n-name">
+                        Polu
+                    </div>
+                </div>
+                <div className="n-right">
+                    <div className="n-lists">
+                        <ul>
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Projects</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                    <label className="switch">
+                        <input type="checkbox" onClick={() => { console.log(event)}} />
+                        <span className="slider round"></span>
+                    </label>
+                    <button className='nav-button'>Hire Me</button>
+                </div>
             </div>
-            <span>Toggle</span>
-        </div>
-        <div className="n-right">
-            <div className="n-lists">
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <button>Hire Me</button>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
